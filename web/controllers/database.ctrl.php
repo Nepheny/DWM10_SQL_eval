d5b07e16663fed7fcecafddbf7fd717c22e3398a
admin <admin@iMac3.local>
2018-05-17 16:58:29 +0200

    Eval finished with bonus and backtip added

--- web/controllers/database.ctrl.php
+++ web/controllers/database.ctrl.php
@@ -7,54 +7,54 @@
     );
 
     function getAllComments($db) {
-        $query = "SELECT * FROM comments";
+        $query = "SELECT * FROM `comments`";
         $comments = $db->query($query);
         $comments = $comments->fetch_all(MYSQLI_ASSOC);
         return $comments;
     }
 
     function orderedComments($db) {
-        $query = "SELECT * FROM comments ORDER BY created_at DESC";
+        $query = "SELECT * FROM `comments` ORDER BY created_at DESC";
         $comments = $db->query($query);
         $comments = $comments->fetch_all(MYSQLI_ASSOC);
         return $comments;
     }
 
     function only10Comments($db) {
-        $query = "SELECT * FROM comments LIMIT 10";
+        $query = "SELECT * FROM `comments` LIMIT 10";
         $comments = $db->query($query);
         $comments = $comments->fetch_all(MYSQLI_ASSOC);
         return $comments;
     }
 
     function adminComments($db) {
-        $query = "SELECT * FROM comments WHERE user='admin'";
+        $query = "SELECT * FROM `comments` WHERE `user`='admin'";
         $comments = $db->query($query);
         $comments = $comments->fetch_all(MYSQLI_ASSOC);
         return $comments;
     }
 
     function removeAdminComments($db) {
-        $query = "DELETE FROM comments WHERE user='admin'";
+        $query = "DELETE FROM `comments` WHERE `user`='admin'";
         $comments = $db->query($query);
         $comments = $comments->fetch_all(MYSQLI_ASSOC);
         return $comments;
     }
 
     function create($db, $user, $content) {
-        $query = $db->prepare("INSERT INTO comments (id, user, content, created_at) VALUES (NULL, ?, ?, CURRENT_TIMESTAMP)");
+        $query = $db->prepare("INSERT INTO `comments` (`id`, `user`, `content`, `created_at`) VALUES (NULL, ?, ?, CURRENT_TIMESTAMP)");
         $query->bind_param("ss", $user, $content);
         $query->execute();
-        $query = "SELECT * FROM comments";
+        $query = "SELECT * FROM `comments`";
         $comments = $db->query($query);
         $comments = $comments->fetch_all(MYSQLI_ASSOC);
         return $comments;
     }
 
     function updateComments($db) {
-        $query = "UPDATE comments SET user='pierre' WHERE user='admin'";
+        $query = "UPDATE `comments` SET `user`='pierre' WHERE `user`='admin'";
         $comments = $db->query($query);
-        $query = "SELECT * FROM comments";
+        $query = "SELECT * FROM `comments`";
         $comments = $db->query($query);
         $comments = $comments->fetch_all(MYSQLI_ASSOC);
         return $comments;
